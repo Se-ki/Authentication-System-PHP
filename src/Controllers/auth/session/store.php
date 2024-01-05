0a3f247940624277b39f58038994903e4ba9ce16
tian-tian <cautor3@gmail.com>
2024-01-06 01:24:25 +0800

    refactored session/store

--- src/Controllers/auth/session/store.php
+++ src/Controllers/auth/session/store.php
@@ -8,56 +8,137 @@ if ($_SERVER['HTTP_SEC_FETCH_SITE'] === "none") {
     exit;
 }
 
-$db = new Database;
-$username = testInput($_POST['username']);
-$password = testInput($_POST['password']);
-$_SESSION['username'] = $username;
-$ip = IPAddress();
-$time = time() - 15;
-$check_login_row = $db->query("SELECT COUNT(*) AS total_count FROM attempts WHERE login_time > :time AND
- ip_address= :ip", [
-    'ip' => $ip,
-    'time' => $time
-])->find();
-$total_count = $check_login_row['total_count'];
-if ($total_count >= 3) {
-    redirect('/logins');
-} else {
-    $user = $db->query("SELECT * FROM users WHERE username = :username", [
-        'username' => $username,
-    ])->find();
-
-    if ($user && password_verify($password, $user['password'])) {
-        $login = true;
-        $_SESSION['user'] = [
-            "id" => $user['id'],
-            "username" => $user['username'],
-            "login" => true
-        ];
-
-        $db->query("DELETE FROM attempts WHERE ip_address= :ip", [
-            'ip' => $ip,
+class Authentication
+{
+    private $username;
+    private $password;
+    private $totalCount;
+    private $ip;
+    private $time;
+    private $db;
+    public function __construct($username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->ip = ipAddress();
+        $this->time = time() - 15;
+        $this->db = new Database;
+    }
+    public function totalAttempt()
+    {
+        $checkLoginRow = $this->db->query("SELECT COUNT(*) AS total_count FROM attempts WHERE login_time > :time AND ip_address= :ip", [
+            'ip' => $this->ip,
+            'time' => $this->time
         ])->find();
-        unset($_SESSION['timeIncrement']);
-        unset($_SESSION['based']);
-        // Session::unflash('username');
-        unset($_SESSION['username']);
-        $_SESSION['username'] = $username;
-        redirect('/home');
-    } else {
-        $login_time = time();
-        $db->query("INSERT INTO attempts VALUES (null, :ip_address, :login_time)", [
-            'ip_address' => $ip,
-            'login_time' => $login_time
+        $this->totalCount =  $checkLoginRow['total_count'];
+        return $this->totalCount;
+    }
+    public function attempt()
+    {
+        if ($this->totalAttempt() >= 3) {
+            redirect('/logins');
+        }
+        $_SESSION['username'] = $this->username;
+        $this->isLogin();
+        if ($this->failedToLogin()) {
+            redirect('/logins');
+        }
+    }
+    public function isLogin()
+    {
+        $user = $this->db->query("SELECT * FROM users WHERE username = :username", [
+            'username' => $this->username,
+        ])->find();
+
+        if ($user && password_verify($this->password, $user['password'])) {
+            $_SESSION['user'] = [
+                "id" => $user['id'],
+                "username" => $user['username'],
+                "login" => true
+            ];
+
+            $this->db->query("DELETE FROM attempts WHERE ip_address= :ip", [
+                'ip' => $this->ip,
+            ])->find();
+            unset($_SESSION['timeIncrement']);
+            unset($_SESSION['based']);
+            unset($_SESSION['username']);
+            $_SESSION['username'] = $this->username;
+            redirect('/home');
+        }
+    }
+    public function failedToLogin()
+    {
+        $loginTime = time();
+        $this->db->query("INSERT INTO attempts VALUES (null, :ip_address, :login_time)", [
+            'ip_address' => $this->ip,
+            'login_time' => $loginTime
         ])->get();
-        $total_count++;
-        $_SESSION['attempts'] = 3 - $total_count;
+        $this->totalCount++;
+        $_SESSION['attempts'] = 3 - $this->totalCount;
         if ($_SESSION['attempts'] <= 0) {
             $_SESSION['based'] += 1;
-            redirect('/logins');
+            return true;
         } else {
             Session::flash('error', "These credentials do not match our records.");
         }
-        redirect('/login');
+        return redirect('/login');
     }
 }
+
+$authentication = new Authentication(testInput($_POST['username']), testInput($_POST['password']));
+$authentication->attempt();
+
+// $db = new Database;
+// $username = testInput($_POST['username']);
+// $password = testInput($_POST['password']);
+// $_SESSION['username'] = $username;
+// $ip = ipAddress();
+// $time = time() - 15;
+// $checkLoginRow = $db->query("SELECT COUNT(*) AS total_count FROM attempts WHERE login_time > :time AND
+//  ip_address= :ip", [
+//     'ip' => $ip,
+//     'time' => $time
+// ])->find();
+// $totalCount = $checkLoginRow['total_count'];
+// if ($totalCount >= 3) {
+//     redirect('/logins');
+// } else {
+//     $user = $db->query("SELECT * FROM users WHERE username = :username", [
+//         'username' => $username,
+//     ])->find();
+
+//     if ($user && password_verify($password, $user['password'])) {
+//         $login = true;
+//         $_SESSION['user'] = [
+//             "id" => $user['id'],
+//             "username" => $user['username'],
+//             "login" => true
+//         ];
+
+//         $db->query("DELETE FROM attempts WHERE ip_address= :ip", [
+//             'ip' => $ip,
+//         ])->find();
+//         unset($_SESSION['timeIncrement']);
+//         unset($_SESSION['based']);
+//         // Session::unflash('username');
+//         unset($_SESSION['username']);
+//         $_SESSION['username'] = $username;
+//         redirect('/home');
+//     } else {
+//         $login_time = time();
+//         $db->query("INSERT INTO attempts VALUES (null, :ip_address, :login_time)", [
+//             'ip_address' => $ip,
+//             'login_time' => $login_time
+//         ])->get();
+//         $totalCount++;
+//         $_SESSION['attempts'] = 3 - $totalCount;
+//         if ($_SESSION['attempts'] <= 0) {
+//             $_SESSION['based'] += 1;
+//             redirect('/logins');
+//         } else {
+//             Session::flash('error', "These credentials do not match our records.");
+//         }
+//         redirect('/login');
+//     }
+// }
